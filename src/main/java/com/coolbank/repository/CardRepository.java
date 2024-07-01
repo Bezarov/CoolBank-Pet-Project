96b774022fe1bcd6e450a1b5252ef1ea7ce59db1
Alexandr Bezarov <bezarov.sasha@gmail.com>
2024-07-01 13:17:26 +0300

    Created repositories interfaces - Account, Card, Payment, User.

--- src/main/java/com/coolbank/repository/CardRepository.java
+++ src/main/java/com/coolbank/repository/CardRepository.java
@@ -1,4 +1,11 @@
 package com.coolbank.repository;
 
-public class CardRepository {
+import com.coolbank.model.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card, UUID> {
 }
--- src/main/java/com/coolbank/repository/PaymentRepository.java
+++ src/main/java/com/coolbank/repository/PaymentRepository.java
@@ -1,4 +1,11 @@
 package com.coolbank.repository;
 
-public class PaymentRepository {
+import com.coolbank.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 }
