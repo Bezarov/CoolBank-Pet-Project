7db1994a41be31fdf2a4edcd9e688ba352ab8089
Alexandr Bezarov <bezarov.sasha@gmail.com>
2024-07-08 17:18:09 +0300

    Card to Card transfer added to PaymentServiceImpl

--- src/main/java/com/coolbank/service/PaymentService.java
+++ src/main/java/com/coolbank/service/PaymentService.java
@@ -3,12 +3,17 @@ package com.coolbank.service;
 import com.coolbank.dto.PaymentDTO;
 import org.springframework.http.ResponseEntity;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface PaymentService {
-    ResponseEntity<String> createPayment(PaymentDTO paymentDTO);
+    ResponseEntity<String> createPaymentFromAccountIdToAccountId(PaymentDTO paymentDTO);
+
+    ResponseEntity<String> createPaymentFromCardNumberToCardNumber(String fromCardNumber,
+                                                                   String toCardNumber,
+                                                                   BigDecimal amount);
 
     PaymentDTO getPaymentById(UUID paymentId);
 
@@ -24,6 +29,7 @@ public interface PaymentService {
     List<PaymentDTO> getAllFromAccountPaymentsByPaymentDateRange(UUID fromAccountId,
                                                                  LocalDateTime fromLocalDateTime,
                                                                  LocalDateTime toLocalDateTime);
+
     List<PaymentDTO> getAllFromToPaymentsByPaymentDateRange(UUID toAccountId,
                                                             LocalDateTime fromLocalDateTime,
                                                             LocalDateTime toLocalDateTime);
