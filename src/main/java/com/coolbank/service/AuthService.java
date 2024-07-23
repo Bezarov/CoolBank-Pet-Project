5493d8e0e3f8879482e7af3fc15f9e470100a898
Alexandr Bezarov <bezarov.sasha@gmail.com>
2024-07-23 17:43:19 +0300

    Created and added realization to AuthController. Created model for UserAuthRequest, ServiceAuthRequest and AuthResponse. Created security packet:         Created SecurityConfig to permit Auth API and User creation.         Created JwtRequestFilter to intercept api requests and check Authentication header.         Created JwtUtil to re/generate, check JWT Token and so on... Created interface AuthService. Created AuthServiceImpl to Authenticate Users, and Service.         added realization for Users. Created UserDetailsServiceImpl to load User details. Changed UsersServiceImpl, to encode all incoming password by Bcrypt and store encrypted in DB. Changed logback setting to store only class as a source of log. Added in pom.xml Security and JWT dependency.

--- src/main/java/com/coolbank/service/AuthService.java
+++ src/main/java/com/coolbank/service/AuthService.java
@@ -1,7 +1,7 @@
 package com.coolbank.service;
 
 public interface AuthService {
-    String authenticateUser(String username, String password);
+    String authenticateUser(String email, String password);
 
     String authenticateService(String serviceId, String serviceSecret);
 }
